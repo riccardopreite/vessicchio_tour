1ad3a6c9d404d70d83c73ea200e183ec5c05d3d6
Davide Davoli <davide.davoli4@studio.unibo.it>
2021-05-14 12:00:34 +0200

    modulo intervalli

---
+++ package/intervals.m
@@ -0,0 +1,123 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*(* :Title:IntervalTheoryAndExercises*)*)
+(*(* :Context:IntervalTheoryAndExercises`*)*)
+(*(* :Author:DD*)*)
+(*(* :Summary: Exercises, theory and tests for learning musical intervals*)*)
+(*(* :Copyright:*)*)
+(*(* :Package Version:1,Maggio 2021*)*)
+(*(* :Mathematica Version:11.2*)*)
+(*(* :History:*)*)
+(*(* :Sources:*)*)
+(*(* :Limitations:*)(* :Discussion:*)*)
+
+
+BeginPackage["IntervalTheoryAndExercises`"];
+Needs["MusicScore`"];
+IntervalTheory::usage ="Prints a panel containing a short tutorial on musical intervals";
+InteractiveInterval::usage ="Displays a tool that allows the user to listen and visualize the notes comoning any interval. It also allows the user to specify the starting interval, by using its name as argument.";
+IntervalExercise::usage="Called without argumets, it displays an exercise in which the user has to listen an interval and guess its name.";
+IntevalTheory[] :=DynamicModule[{intervals, images, captions, buttons, y},
+intervals = {"Prima", "Seconda", "Terza", "Quarta", "Quinta", "Sesta", "Settima", "Ottava"};
+images = {PrintPart[{{"C4"}, {"C4"}}],  PrintPart[{{"C4"}, {"D4"}}], PrintPart[{{"C4"}, {"E4"}}], PrintPart[{{"C4"}, {"F4"}}], PrintPart[{{"C4"}, {"G4"}}], PrintPart[{{"C4"}, {"A4"}}], PrintPart[{{"C4"}, {"B4"}}], PrintPart[{{"C4"}, {"C5"}}]};
+
+buttons={{Button["Riproduci unisono", EmitSound[Sound[{SoundNote["C4"], SoundNote["C4"]}]]]},
+
+ {Button["Riproduci M2", EmitSound[Sound[{SoundNote["C4"], SoundNote["D4"]}]]], Button["Riproduci m2", EmitSound[Sound[{SoundNote["C4"], SoundNote["Db4"]}]]]}, 
+ 
+{Button["Riproduci M3", EmitSound[Sound[{SoundNote["C4"], SoundNote["E4"]}]]], Button["Riproduci m3", EmitSound[Sound[{SoundNote["C4"], SoundNote["Eb4"]}]]]}, 
+
+{Button["Riproduci P4", EmitSound[Sound[{SoundNote["C4"], SoundNote["F4"]}]]], Button["Riproduci A4", EmitSound[Sound[{SoundNote["C4"], SoundNote["F#4"]}]]]}, 
+
+{Button["Riproduci P5", EmitSound[Sound[{SoundNote["C4"], SoundNote["G4"]}]]], Button["Riproduci d5", EmitSound[Sound[{SoundNote["C4"], SoundNote["Gb4"]}]]]}, 
+
+{Button["Riproduci M6", EmitSound[Sound[{SoundNote["C4"], SoundNote["A4"]}]]], Button["Riproduci m6", EmitSound[Sound[{SoundNote["C4"], SoundNote["Ab4"]}]]]}, 
+
+{Button["Riproduci M7", EmitSound[Sound[{SoundNote["C4"], SoundNote["B4"]}]]], Button["Riproduci m7", EmitSound[Sound[{SoundNote["C4"], SoundNote["Bb4"]}]]]}, 
+
+{Button["Riproduci P8", EmitSound[Sound[{SoundNote["C4"], SoundNote["C5"]}]]]} 
+
+};
+captions = {"L'intervallo pi\[UGrave] semplice \[EGrave] detto \"unisono\" ed \[EGrave] quello che intercorre fra due note che hanno lo stesso nome,come,ad esempio,due Do.
+Questo intervallo \[EGrave] un intervallo perfetto,per cui la sua sigla \[EGrave] \"P1\"",
+
+"Il secondo intervallo, in ordine di distanza, \[EGrave] detto \"seconda\", esistono due specie di intervalli di seconda: 
+
+- Maggiore, \"M2\" (come fra un Do ed un Re della scala maggiore).
+- Minore, \"m2\" (come fra un Do ed un Re bemolle o un Do diesis).",
+
+"Anche l'intervallo di terza esiste in due specie: maggiore o minore. La terza maggiore, \"M3\", \[EGrave] l'intervallo che c'\[EGrave] fra un Do ed un Mi (o fra un Sol ed un Si, per esempio). La terza minore, \"m3\" \[EGrave] l'intervallo che c'\[EGrave] fra un Do ed un Mi bemolle (o fra un Re ed un Fa, od un La ed un Do, per esempio).",
+ "L'intervallo di quarta esiste in una sola specie: la quarta perfetta (o giusta), che si rappresenta con la sigla \"P4\". Sulla scala di Do, l'intervallo di quarta perfetta \[EGrave] quello che c'\[EGrave] fra il Do ed il Fa. L'intervallo di quarta diminuita \"d4\", coicide con l'intervallo di terza maggiore, per cui, se non necessario per ragioni armoniche, non viene quasi mai utilizzato. Invece, l'intervallo di quarta aumentata, \"A4\", invece esiste ed \[EGrave] il cosiddetto \"tritono\", un intervallo a lungo avversato e quasi demonizzato per secoli a causa della sua fortissima dissonanza. Sulla scala di Do, l'intervallo di quarta aumentata \[EGrave], ad esempio, l'intervallo fra il Do ed il Fa#, oppure quello fra il Si ed il Fa.",
+ "Anche l'intervallo di quinta \[EGrave] perfetto: la sua sigla \[EGrave] \"P5\". Nella scala di Do, l'intervallo di quinta \[EGrave] quello che c'\[EGrave] fra il Do ed il Sol, o fra il Re ed il La, eccetera. \[CapitalEGrave] caratterizzato da una fortissima consonanza. L'intervallo di quinta diminuita, \"d5\" chiaramente coincide con quello di quarta aumentata, mentre l'intervallo di quinta aumentata, \"A5\", coincide con quello di sesta minore \"m6\", ed \[EGrave] caratterizzato da una fortissima dissonanza.",
+
+"L'intervallo di sesta pu\[OGrave] essere maggiore o minore. La sesta maggiore, \"M6\" \[EGrave] un intervallo molto consonante e brillante, mentre la sesta minore \"m6\" (che coincide con la quinta aumentata \"A5\") \[EGrave] un intervallo molto cupo e dissonante. Sulla scala di Do, l'intervallo di sesta maggiore si trova fra il Do ed il La.",
+"L'intervallo di settima pu\[OGrave] essere maggiore o minore. La settima maggiore, \"M7\", \[EGrave] anche detto \"sensibile\" perch\[EAcute] trasmette un forte senso di precariet\[AGrave] ed una ceta dissonanza poich\[EAcute] \[EGrave] molto vicino all'intervallo di ottava. Sulla scala di Do, l'intervallo di sesta \[EGrave] quello fra il Do ed il Si. La settima minore, invece trasmette un senso di risoluzione imminente. Sulla scala di Do, \[EGrave] la distanza fra il Do ed il Si bemolle.",
+"L'intervallo di ottava \[EGrave] un intervallo perfetto e si indica con la sigla \"P8\". In ogni scala \[EGrave] l'intrvallo fra due note consecutive con lo stesso nome. 
+L'intervallo di ottava diminuita \"d8\" coincide con l'intervallo di settima maggiore \"M7\"."
+
+};
+
+(*Manipulate[Part[instruments,n], {n, 1, 5, 1}];
+Manipulate[ActionMenu["click",{"one"\[RuleDelayed]( Print[instruments[[1]]]),"two"\[RuleDelayed](x=2)},Appearance\[Rule]stru],{stru,{Automatic,"PopupMenu","Button","Framed",None}}];*)
+Column[{
+Panel[Row[{SetterBar[Dynamic[y], intervals ]}],Alignment-> Center, ImageSize->{Full, Automatic} ],
+Panel[Dynamic[GraphicsRow[{Magnify[images[[First[First[Position[intervals,y]]]]], 1], Column[{Text[Style[captions[[First[First[Position[intervals,y]]]]], LineIndent-> 0, FontFamily->"Arial", FontSize->16]], GraphicsRow[buttons[[First[First[Position[intervals,y]]]]]]}]}, ImageSize->{Full, Automatic}]],FrameMargins->50, ImageSize->{Full, Automatic}]}
+]
+]
+
+InteractiveInterval[aa_/; ContainsAny[AllIntervals, {aa}]]:= DynamicModule[{interval, basenote, sign, allnotes},
+allnotes=Take[AllNotes, {24, -24}];
+basenote=RandomChoice[allnotes];
+interval=aa;
+sign="+";
+Panel[Column[{
+Dynamic[Magnify[PrintPart[{{basenote}, {AddInterval[basenote, interval, sign]}}], 4]],
+Row[{TextCell["Intervallo: "], InputField[Dynamic[interval], String], Button["Randomize", interval=RandomChoice[AllIntervals]]}],
+Row[{TextCell["Nota base: "],InputField[Dynamic[basenote], String], Button["Randomize", basenote = RandomChoice[allnotes]]}],
+Row[{TextCell["Verso (+/-): "],InputField[Dynamic[sign], String], Button["Randomize", sign = RandomChoice[{"+", "-"}]]}],
+Row[{Button["Play", EmitSound[Sound[{SoundNote[basenote], SoundNote[AddInterval[basenote, interval, sign]]}]]]}]
+}
+]]
+]
+
+IntervalExercise[]:= DynamicModule[{interval, basenote, sign, allnotes, guessedinterval, msg, part, cintervals , mMintervals, simpleintervals},
+allnotes=Take[AllNotes, {24, -24}];
+allnotes=Select[allnotes, StringContainsQ[#, "#"]==False&];
+basenote=RandomChoice[allnotes];
+mMintervals=Select[AllIntervals,(Characters[#][[1]]=="m" || Characters[#][[1]]=="M" ||  Characters[#][[1]]=="P")&];
+Dynamic[msg];
+msg="";
+Dynamic[guessedinterval];
+simpleintervals={"M2", "M3", "M4", "P5", "P1", "P8"};
+cintervals=simpleintervals;
+interval=RandomChoice[cintervals];
+sign=RandomChoice[{"+", "-"}];
+part=PrintPart[{{basenote}}];
+
+Panel[Column[{
+Row[{TextCell["Difficolt\[AGrave]: "], PopupMenu[Dynamic[cintervals],{
+simpleintervals-> "Facile",
+simpleintervals-> "Intermedio",
+simpleintervals-> "Difficile" }]}],
+Dynamic[Magnify[part, 4]],
+Row[{TextCell["Intervallo: "], InputField[Dynamic[guessedinterval], String], 
+Button["Controlla", 
+If[guessedinterval==  interval, msg="Risposta corretta";part=PrintPart[{{basenote}, {AddInterval[basenote, interval, sign]}}];,msg="Risposta errata"; ];
+]}],
+Row[{Button["Play", EmitSound[Sound[{SoundNote[basenote], SoundNote[AddInterval[basenote, interval, sign]]}]]], 
+Button["Nuovo esercizio", basenote=RandomChoice[allnotes];
+interval=RandomChoice[cintervals];
+sign=RandomChoice[{"+", "-"}];
+part=PrintPart[{{basenote}}]; 
+msg=""; ]}],
+Row[{TextCell[Dynamic[msg]]}]
+}
+]]
+]
+End[]
+EndPackage[]
+
+
+(* ::Input:: *)
+(**)
--- package/print_music_score.m
+++ package/print_music_score.m
@@ -19,6 +19,19 @@ BeginPackage["MusicScore`"];
 
 PrintPart::usage="PrintPart [notesList_]
 Create a music score from list notes in noteList, they have to be sub list with a number next to note e.g. {{notes..},{notes..}}." ;
+AmNatList::usage ="List of notes in American notation, without sharp notes";
+AmSharpList::usage = "List of notes in American notation, with sharp notes";
+Sharpize ::usage ="Given a note name n that possibly contains a flat mark, it converts it in its equivalent note but with sharp. Eg. Sharpize[\"Bb\"] wil return \"A#\". ";
+Bequadro::usage ="Given a note name n that can contain a flat mark or a sharp mark it computes the function off applying a bequadro to it, i.e. removing the alteration. For exaple Bequadro[\"C##4\"] will return \"C4\".";
+AllNotes::usage="List of all 88 notes in american notation with octave index as trird elements. Eg. \"A#4\"";
+NoteDistance::usage ="Given any two notes in American notation, possibly with flats and sharps, it returns their distance expressed in semitones";
+NoteSpaces::usage="Given two notes in American notation, it returns their distance expressed in rows (spaces or rows) on the staff.";
+NotePreorderQ::usage= "Given any two notes in American notation, it returns a true value if and only if the first is lower than the second. For example NotePreorderQ[\"C0\", \"D0\"] returns true" ;
+NoteSpacesPreorderQ::usage= "Given any two notes in American notation, it returns a true value if and only if the number of spaceson the staff between the two notes il lower than 0. For example NotePreorderQ[\"C#5\", \"C0\"] returns true because the two notes are both placed between the same rows." ;
+PrintPart::usage = "Given a list of lists of notes, it represents the notes that are in the same SUB-list vertically aligned (as a chord) horizontally separed by the other sublists. For example
+PrintPart[{{\"D4\", \"F4\", \"G4\", \"B4\"}, {\"C4\", \"E4\", \"G4\", \"C4\"}}] will print a nice ending :-).";
+IntervalSemitones::usage ="Given the name of an interval, it will return the distance, in semitones between the two notes of the interval. For example IntervalSemitones[\"P8\"] will return 12.";
+AddInterval::usage = "Given a note, an interval and a sign (in {\"+\", \"-\"}), it will compute the result of adding the interval to the note. For example AddInterval[\"C4\", \"P8\", \"-\"] will return \"C3\".";
 
 $path = StringReplace[NotebookDirectory[],"package\\" -> ""];
 $chiave = Import[$path <>"immagini\\chiave.png"];
@@ -35,51 +48,65 @@ $AmSharpList ={"C","C#", "D", "D#",  "E", "F", "F#", "G", "G#",  "A", "A#",  "B"
 Begin["`Private`"];
 
 
+AllNotes =Join[{"A0", "A#0","B0"},  Flatten[Table [StringJoin[note, IntegerString[number]],  {number, 1, 7, 1}, {note, $AmSharpList}]], {"C8"}];
+
+AllIntervals=Flatten[Map[#[[1]]&, IntervalMap]];
+AddInterval[n_, i_, a_]:=None; 
+AddInterval[n_, i_/;ContainsAny[AllIntervals, {i}], a_/; a=="+" || a=="-"] := AllNotes[[Position[AllNotes, Sharpize[n]][[1]][[1]]+If[a!="-", IntervalSemitones[i], -IntervalSemitones[i]]]];
+
+Bequadro[n_/; StringLength[n]>2]:= StringJoin[StringTake[n,{1, 1}], StringTake[n, -1]]; 
+Bequadro[n_/; StringLength[n]<=2]:= n; 
+
+NoteDistance[a_, b_:"C4"] := Position[AllNotes,Bequadro[a]][[1]][[1]]- Position[AllNotes,Bequadro[b]][[1]][[1]]+If[StringContainsQ[a, "#"] || StringContainsQ[b, "b"], 1, 0]+If[StringContainsQ[b, "#"] || StringContainsQ[a, "b"],- 1, 0];
+
 Sharpize[n_]:=n;
-Sharpize[n_/; StringTake[n, -1]=="b"] := StringJoin[$AmNatList[[Mod[Position[$AmNatList, StringTake[n, 1]][[1]][[1]]+5, 7]+1]], "#"];
+Sharpize[n_/; n != "Cb" && n != "Fb" &&StringTake[n, -1]=="b"] := StringJoin[$AmNatList[[Mod[Position[$AmNatList, StringTake[n, 1]][[1]][[1]]+5, 7]+1]], "#"];
+Sharpize[n_/; n=="Fb" ] := "E";
+Sharpize[n_/; n=="Cb" ] := "B";
 
 NoteImg[n_/; !StringContainsQ[n,"#"] && !StringContainsQ[n,"b"], img_ :$note]:=ImageCompose[$blank, img, {119, 84.5}];
-NoteImg[n_/;StringContainsQ[n, "#"] , img_ :$note] := ImageCompose[ImageCompose[$blank, $note, {119, 84.5}], $diesis, {70, 83}];
-NoteImg[n_/;StringContainsQ[n, "b"] , img_ :$note] := ImageCompose[ImageCompose[$blank, $note, {119, 84.5}], $bemolle, {70, 100}];
-
-NoteDistance[a_, b_:"C4"] := 12FromDigits[StringTake[a, -1]]- 12FromDigits[StringTake[b, -1]] +Position[$AmSharpList, Sharpize[StringTake[a, {1,-2}]]][[1]][[1]]- Position[$AmSharpList,Sharpize[StringTake[b, {1,-2}]]][[1]][[1]];
+NoteImg[n_/;StringContainsQ[n, "#"] , img_ :$note] := ImageCompose[ImageCompose[$blank, img, {119, 84.5}], $diesis, {70, 83}];
+NoteImg[n_/;StringContainsQ[n, "b"] , img_ :$note] := ImageCompose[ImageCompose[$blank, img, {119, 84.5}], $bemolle, {70, 100}];
 
 NoteSpaces[a_, b_:"C4"] := 7FromDigits[StringTake[a, -1]]- 7FromDigits[StringTake[b, -1]] +Position[$AmNatList, StringTake[a, 1]][[1]][[1]]- Position[$AmNatList, StringTake[b, 1]][[1]][[1]];
-
 NotePreorderQ[a_, b_:"C4"] := NoteDistance[a, b]<=0;
 
 NoteSpacesPreorderQ[a_, b_:"C4"] := NoteSpaces[a, b]<=0;
 
-PrintNote[o_, n_/;NotePreorderQ[n, "G5"]&& !NotePreorderQ[n,"C4"], back_:$chiave]:=ImageCompose[back, NoteImg[n,$note],  {300+150*o,372+13.2*NoteSpaces[n, "D4"] }];
-PrintNote[ o_, n_/;NotePreorderQ[n, "B3"]&& !NotePreorderQ[n,"E2"], back_:$chiave]:=ImageCompose[back, NoteImg[n,$note], {300+150*o,104+13.2*NoteSpaces[n, "F2"] }];
-PrintNote[ o_, n_/;NotePreorderQ[n, "C4"]&& NotePreorderQ["C4",n], back_:$chiave]:=ImageCompose[back, NoteImg[n,$notebarra], {300+150*o,304}];
+PrintNote[o_, n_/;SameQ[n, None]==False &&NoteSpacesPreorderQ[n, "G5"]&& !NoteSpacesPreorderQ[n,"C4"], back_:$chiave]:=ImageCompose[back, NoteImg[n,$note],  {300+150*o,372+13.2*NoteSpaces[n, "D4"] }];
+PrintNote[ o_, n_/;SameQ[n, None]==False &&NoteSpacesPreorderQ[n, "B3"]&& !NoteSpacesPreorderQ[n,"E2"], back_:$chiave]:=ImageCompose[back, NoteImg[n,$note], {300+150*o,104+13.2*NoteSpaces[n, "F2"] }];
+PrintNote[ o_, n_/;SameQ[n, None]==False &&NoteSpacesPreorderQ[n, "C4"]&& NoteSpacesPreorderQ["C4",n], back_:$chiave]:=ImageCompose[back, NoteImg[n,$notebarra], {300+150*o,304}];
 
-PrintNote[o_, n_/;NotePreorderQ["Ab5", n]&&Mod[NoteSpaces[n, "G5"], 2]==1, back_:$chiave]:=FoldList[
+PrintNote[o_, n_/;SameQ[n, None]==False &&NoteSpacesPreorderQ["Ab5", n]&&Mod[NoteSpaces[n, "G5"], 2]==1, back_:$chiave]:=FoldList[
 ImageCompose[#1, $barra,  {320+150*o,473.8+13.2*#2 }]&,
 ImageCompose[back, NoteImg[n,$notebarra],  {300+150*o,372+13.2*NoteSpaces[n, "D4"] }],
 Range[2, NoteSpaces[n, "F5"]-2, 2]][[-1]];
 
-PrintNote[o_, n_/;NotePreorderQ["Ab5", n]&&Mod[NoteSpaces[n, "G5"], 2]==0, back_:$chiave]:=FoldList[
+PrintNote[o_, n_/;SameQ[n, None]==False &&NoteSpacesPreorderQ["Ab5", n]&&Mod[NoteSpaces[n, "G5"], 2]==0, back_:$chiave]:=FoldList[
 ImageCompose[#1, $barra,  {320+150*o,473.8+13.2*#2 }]&,
 ImageCompose[back, NoteImg[n,$note],  {300+150*o,372+13.2*NoteSpaces[n, "D4"] }],
 Range[2, NoteSpaces[n, "F5"], 2]][[-1]];
 
-PrintNote[o_, n_/;NotePreorderQ[n, "Fb2"]&&Mod[NoteSpaces[n, "F2"], 2]==1, back_:$chiave]:=FoldList[
+PrintNote[o_, n_/;SameQ[n, None]==False && NoteSpacesPreorderQ[n, "Fb2"]&&Mod[NoteSpaces[n, "F2"], 2]==1, back_:$chiave]:=FoldList[
 ImageCompose[#1, $barra,  {320+150*o,87-13.2*(#2-1) }]&,
-ImageCompose[back, NoteImg[n,$notebarra],  {300+150*o,104-13.2*NoteSpaces["F2", n] }],
+ImageCompose[back, NoteImg[n,$notebarra ],  {300+150*o,104-13.2*NoteSpaces["F2", n] }],
 Range[2, NoteSpaces["F2", n], 2]][[-1]];
 
-PrintNote[o_, n_/;NotePreorderQ[n, "Fb2"]&&Mod[NoteSpaces[n, "F2"], 2]==0, back_:$chiave]:=FoldList[
+PrintNote[o_, n_/;SameQ[n, None]==False &&NoteSpacesPreorderQ[n, "Fb2"]&&Mod[NoteSpaces[n, "F2"], 2]==0, back_:$chiave]:=FoldList[
 ImageCompose[#1, $barra,  {320+150*o,87-13.2*(#2-1) }]&,
 ImageCompose[back, NoteImg[n,$note],  {300+150*o,104-13.2*NoteSpaces["F2", n] }],
 Range[2, NoteSpaces["F2", n], 2]][[-1]];
 
+PrintNote[o_, n_/;SameQ[n, None], back_:$chiave]:=back;
 
 PrintChord[o_, c_, back_:$chiave] :=Fold[PrintNote[o, #2, #1]&, back, c];
 PrintPartPre[p_,o_:0, back_:$chiave] := Fold[{#1[[1]]+1, PrintChord[#1[[1]], #2, #1[[2]] ]}&, { 0, back}, p  ];
-
-
 PrintPart[p_] := PrintPartPre[p, 0, $chiave][[2]];
+
+IntervalMap={{"P1", 0}, {"m2", 1}, {"M2", 2}, {"m3", 3}, {"M3", 4}, {"P4", 5}, {"A4", 6}, {"d5", 6}, {"D5", 6}, {"P5", 7}, {"m6", 8}, {"A5", 8}, {"M6", 9}, {"m7", 10}, {"M7", 11}, {"P8", 12}};
+
+IntervalSemitones[i_]:=IntervalMap[[Position[IntervalMap, {i, _}][[1]][[1]]]][[2]];
+
 
 End[]
 EndPackage[]
